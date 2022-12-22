31be6a998a35aff7eb4677c1758680b8ab9b3cc3
silinushka <silinushka2@gmail.com>
2022-12-22 18:52:22 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let x = Number(prompt("Enter x"));
+let y = Number(prompt("Enter y"));
+
+alert ("x + y = " + (x + y));
+alert ("x - y = " + (x - y));
+alert ("x * y = " + (x * y));
+alert ("x / y = " + (x / y));
